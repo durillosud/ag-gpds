64990e6c3af24ebf3e49a63ba12c84daa702ae1e
Danyllo S C <danyllo.cardoso@ueg.br>
2023-06-29 06:02:48 -0300

    Added first web resource

---
+++ src/main/java/br/com/gpds/domain/DomainConstants.java
@@ -0,0 +1,6 @@
+package br.com.gpds.domain;
+
+public class DomainConstants {
+    public static final String API_PATH = "/api/gpds";
+    public static final String CUSTOMERS = "/clientes/";
+}
---
+++ src/main/java/br/com/gpds/web/rest/ClientesResource.java
@@ -0,0 +1,44 @@
+package br.com.gpds.web.rest;
+
+
+import br.com.gpds.domain.ClientesEntity;
+import br.com.gpds.domain.DomainConstants;
+import br.com.gpds.web.rest.errors.BadRequestAlertException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping(DomainConstants.API_PATH)
+@Validated
+public class ClientesResource {
+
+    @GetMapping(value = DomainConstants.CUSTOMERS + "listar", produces = { "application/json" })
+    @Operation(
+        summary = "Lista paginada com clientes",
+        description = "Lista paginada com clientes:<br/>" +
+            "<ul><li>filtro por `nome`;</li><br/>" +
+            "<li>paginação `nº da página`, `tamanho da página`, `campo de ordenação`, `tipo de ordenação`;</li></ul>",
+        operationId = "getAllClientesPaginated",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+        }
+    )
+    public Page<ClientesEntity> getAllClientesPaginated(
+        UriComponentsBuilder uriBuilder,
+        HttpServletResponse response
+    ) throws ErrorResponseException {
+        try {
+            return Page.empty();
+        } catch (ErrorResponseException e) {
+            throw new BadRequestAlertException("message", "entity", "error");
+        }
+    }
+}
